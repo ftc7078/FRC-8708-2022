d3f2933bef3a83132f2fa6c146b19c38255b8477
ftc7078 <59035211+ftc7078@users.noreply.github.com>
2022-02-14 17:24:27 -0500

    Initial checkin of early test code

---
+++ src/main/java/frc/robot/vision/MyVisionThread.java
@@ -0,0 +1,51 @@
+package frc.robot.vision;
+
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.cscore.CvSink;
+import edu.wpi.first.cscore.CvSource;
+import edu.wpi.first.cscore.UsbCamera;
+
+public class MyVisionThread extends Thread {
+
+
+    public void run() {
+        UsbCamera camera = CameraServer.startAutomaticCapture();
+        // Set the resolution
+        camera.setResolution(640, 480);
+
+        // Get a CvSink. This will capture Mats from the camera
+        CvSink cvSink = CameraServer.getVideo();
+        // Setup a CvSource. This will send images back to the Dashboard
+        CvSource outputStream = CameraServer.putVideo("Rectangle", 640, 480);
+
+        // Mats are very memory expensive. Lets reuse this Mat.
+        Mat mat = new Mat();
+        SkystoneDetector stoneFinder = new SkystoneDetector();
+     
+        while (!Thread.interrupted()) {
+            // Tell the CvSink to grab a frame from the camera and put it
+            // in the source mat. If there is an error notify the output.
+            if (cvSink.grabFrame(mat) == 0) {
+                // Send the output the error.
+                outputStream.notifyError(cvSink.getError());
+                // skip the rest of the current iteration
+                continue;
+            }
+            // Put a rectangle on the image
+
+            //Imgproc.cvtColor(input, mat, Imgproc.COLOR_RGB2HSV);
+            Mat output = stoneFinder.processFrame(mat);
+
+            //Imgproc.rectangle(mat, new Point(100, 100), new Point(400, 400), new Scalar(255, 255, 255), 5);
+
+            // Give the output stream a new image to display
+            outputStream.putFrame(output);
+        }
+    }
+
+}
