80221d65b1b2834d102217f987837cde60af06d8
ftc7078 <59035211+ftc7078@users.noreply.github.com>
2022-03-25 19:48:37 -0400

    Revert "Circle Detection"
    This reverts commit 69a73713d84458fa35077a97662c30222192c5c5.

--- src/main/java/frc/robot/vision/SkystoneDetector.java
+++ src/main/java/frc/robot/vision/SkystoneDetector.java
@@ -13,12 +13,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SkystoneDetector {
-    /*enum SkystoneLocation {
-    *    LEFT,
-    *   RIGHT,
-    *    NONE
-    *}
-    */
+    enum SkystoneLocation {
+        LEFT,
+        RIGHT,
+        NONE
+    }
+
     private int width; // width of the image
     SkystoneLocation location;
 
@@ -26,9 +26,11 @@ public class SkystoneDetector {
      *
      * @param width The width of the image (check your camera)
      */
-    public void SkystoneDetector(int width) {this.width = width;}
+    public void SkystoneDetector(int width) {
+        this.width = width;
+    }
 
-    public Mat processFrame(Mat input, int color) {
+    public Mat processFrame(Mat input) {
         // "Mat" stands for matrix, which is basically the image that the detector will process
         // the input matrix is the image coming from the camera
         // the function will return a matrix to be drawn on your phone's screen
@@ -41,23 +43,17 @@ public class SkystoneDetector {
         Mat mat = new Mat();
         Imgproc.cvtColor(input, mat, Imgproc.COLOR_RGB2HSV);
 
-        // if something is wrong, we assume there's no ball
+        // if something is wrong, we assume there's no skystone
         if (mat.empty()) {
             location = SkystoneLocation.NONE;
             return input;
         }
 
-        // We create a HSV range for both colors in an if statement to decide on which team we are to detect balls
+        // We create a HSV range for yellow to detect regular stones
         // NOTE: In OpenCV's implementation,
         // Hue values are half the real value
-        if (color == 0) { //Blue color array
-            Scalar lowHSV = new Scalar(150, 150, 0); // lower bound HSV for blue
-            Scalar highHSV = new Scalar(180, 255, 255); // higher bound HSV for blue
-        }
-        else{ //red color array
-            Scalar lowHSV = new Scalar(0, 150, 0); // lower bound HSV for red
-            Scalar highHSV = new Scalar(10, 255, 255); // higher bound HSV for red
-        }
+        Scalar lowHSV = new Scalar(20, 100, 100); // lower bound HSV for yellow
+        Scalar highHSV = new Scalar(50, 255, 255); // higher bound HSV for yellow
         Mat thresh = new Mat();
 
         // We'll get a black and white image. The white regions represent the regular stones.
@@ -78,41 +74,40 @@ public class SkystoneDetector {
 
         MatOfPoint2f[] contoursPoly  = new MatOfPoint2f[contours.size()];
         Rect[] boundRect = new Rect[contours.size()];
-        Point[] centers = new Point[contours.size()];
-        float[][] radius = new float[contours.size()][1];
         for (int i = 0; i < contours.size(); i++) {
             contoursPoly[i] = new MatOfPoint2f();
             Imgproc.approxPolyDP(new MatOfPoint2f(contours.get(i).toArray()), contoursPoly[i], 3, true);
             boundRect[i] = Imgproc.boundingRect(new MatOfPoint(contoursPoly[i].toArray()));
-            centers[i] = new Point();
-            Imgproc.minEnclosingCircle(contoursPoly[i], centers[i], radius[i]);
         }
-        Mat drawing = Mat.zeros(cannyOutput.size(), CvType.CV_8UC3);
-        List<MatOfPoint> contoursPolyList = new ArrayList<>(contoursPoly.length);
-        for (MatOfPoint2f poly : contoursPoly) {
-            contoursPolyList.add(new MatOfPoint(poly.toArray()));
-        }
-        for (int i = 0; i < contours.size(); i++) {
-            if (color == 0) { //blue circle
-                Scalar color = new Scalar(0, 0, 255);
-            }
-            else{ //red circle
-                Scalar color = new Scalar(255, 0, 0);
-            }
-            Imgproc.drawContours(drawing, contoursPolyList, i, color);
-            Imgproc.circle(drawing, centers[i], (int) radius[i][0], color, 2);
+        Imgproc.cvtColor(mat, input, Imgproc.COLOR_HSV2RGB);
+
+        // Iterate and check whether the bounding boxes
+        // cover left and/or right side of the image
+        double left_x = 0.25 * width;
+        double right_x = 0.75 * width;
+        boolean left = false; // true if regular stone found on the left side
+        boolean right = false; // "" "" on the right side
+        for (int i = 0; i != boundRect.length; i++) {
+            if (boundRect[i].x < left_x)
+                left = true;
+            if (boundRect[i].x + boundRect[i].width > right_x)
+                right = true;
+
+            // draw red bounding rectangles on mat
+            // the mat has been converted to HSV so we need to use HSV as well
+            Imgproc.rectangle(input, boundRect[i], new Scalar(0, 0, 255));
         }
 
         // if there is no yellow regions on a side
         // that side should be a Skystone
-        //if (!left) location = SkystoneLocation.LEFT;
-        //else if (!right) location = SkystoneLocation.RIGHT;
+        if (!left) location = SkystoneLocation.LEFT;
+        else if (!right) location = SkystoneLocation.RIGHT;
         // if both are true, then there's no Skystone in front.
         // since our team's camera can only detect two at a time
         // we will need to scan the next 2 stones
-        //else location = SkystoneLocation.NONE;
+        else location = SkystoneLocation.NONE;
 
-        return input; // return the mat with circles drawn
+        return input; // return the mat with rectangles drawn
     }
 
     public SkystoneLocation getLocation() {
