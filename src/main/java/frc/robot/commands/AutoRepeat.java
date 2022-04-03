b32f10ecdf9a73cb40b96a6df2d6020e32af6873
ftc7078 <59035211+ftc7078@users.noreply.github.com>
2022-04-03 14:57:37 -0400

    Autosave Issue

--- src/main/java/frc/robot/commands/AutoRepeat.java
+++ src/main/java/frc/robot/commands/AutoRepeat.java
@@ -1,17 +1,21 @@
 package frc.robot.commands;
 
 
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.ShooterSimple;
 
-public class ShootUnused extends SequentialCommandGroup { 
-    public ShootUnused(ShooterSimple m_shooter) {
-        addCommands(
-            new InstantCommand(m_shooter::runFeeder, m_shooter),
-            new WaitCommand(2),
-            new InstantCommand(m_shooter::stopFeeder, m_shooter));
+public class AutoRepeat extends CommandBase { 
+    private long lastPressed = 0;
+    public AutoRepeat(ShooterSimple m_shooter) {
+        long time = System.currentTimeMillis();
+        if (time > lastPressed +  20) {
+            m_shooter.faster();
+        }
+
+
       }
     
 }
